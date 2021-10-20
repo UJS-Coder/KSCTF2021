875776714541c47f632b23366415333a63a3cd76
KSroido <ksxmyqj@gmail.com>
2021-10-20 19:11:55 +0800

    update

---
+++ RE/XOR/xor.cpp
@@ -0,0 +1,18 @@
+﻿#include <cstdio>
+#include <unistd.h>
+#include<iostream>
+#include <stdlib.h>
+#include<string>
+using namespace std;
+
+
+int main() {
+	string a = "FLAG[xorISeaSY]";
+	for (size_t i = 0; i < a.length(); i++)
+	{
+		a[i] ^= 32; //对32取异或可以用来转换大小写
+	}
+	cout << a;
+
+	return 0;
+}
